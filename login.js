21c1eb4864fdeea2533a041de1f5c3d2892722c2
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-21 10:34:42 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("myPsw").value;
+  document.getElementById("password").innerHTML = x;
+}
+
+if (document.getElementById("password") = yourmom) {
+  alert("right password");
+}
